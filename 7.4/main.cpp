39c3c115a6227ab1e8f47eac162f0313a57a9e9b
daomin-xsy <51433504+daomin-xsy@users.noreply.github.com>
2019-07-03 21:06:35 +0800

    Initial Commit

---
+++ 7.4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  7.4
+//
+//  Created by s20181105303 on 2019/7/3.
+//  Copyright © 2019 s20181105303. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
